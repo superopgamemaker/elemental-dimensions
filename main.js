0aa492b531de4ddaf22b585c78de178d782717c6
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2020-02-29 10:53:04 -0800

    nuclei
    replacing the "hydrogen" with hydrogen nuclei

--- main.js
+++
@@ -1,9 +0,0 @@
-var hydrogen = 0
-
-function gameLoop() {
-hydrogen+=0.1
-hydrogen = Math.round( hydrogen * 10) / 10
-document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogen + " hydrogen."
-}
-
-setInterval(gameLoop, 100)
