d1cb83203ff2cc331419b0947ac991b7fe8b2678
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2020-02-29 11:33:52 -0800

    fixxed
    hopefully

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var hydrogenNuclei = 10
 
 function gameLoop() {
-hydrogenNuclei+=0.001
-hydrogenNuclei = Math.round( hydrogenNuclei * 1000) / 1000
+hydrogenNuclei+=0.01
+hydrogenNuclei = Math.round( hydrogenNuclei * 100) / 100
 document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogenNuclei + " hydrogen nuclei."
 }
 
-setInterval(gameLoop, 1)
+setInterval(gameLoop, 10)
