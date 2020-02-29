0e3c95e241423c3fd0dd9223b558663325be44ee
NGCMan <nintendogamecubeman@gmail.com>
2020-02-29 11:03:22 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var hydrogenNuclei = 0
 function gameLoop() {
 hydrogenNuclei+=0.1
 hydrogenNuclei = Math.round( hydrogenNuclei * 10) / 10
-document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogen + " hydrogen nuclei."
+document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogenNuclei + " hydrogen nuclei."
 }
 
 setInterval(gameLoop, 100)
