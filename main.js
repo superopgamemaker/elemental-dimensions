64a26fdbe57b74f01d1cff3c32f75f760e842d5d
NGCMan <nintendogamecubeman@gmail.com>
2020-02-29 10:42:45 -0800

    0.1/s instead of 1/s

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var hydrogen = 0
 
 function gameLoop() {
-hydrogen++
-document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogen + "hydrogen."
+hydrogen+=0.1
+hydrogen = Math.round( hydrogen * 10) / 10
+document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogen + " hydrogen."
 }
 
 setInterval(gameLoop, 100)
-
