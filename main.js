36610e74361a2db54fe2958625b1d7b5d31d227c
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2020-03-12 19:09:17 -0700

    wait, WHERE IS THE FREAKING BALANCE?

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ var firstHDCost = 10
 var firstHDMultiplier = 1
 var firstHDTo10 = 0
 var firstHydrogenDimension = 0
+var secondHDCost = 100
+var secondHDMultiplier = 1
+var secondHDTo10 = 0
+var secondHydrogenDimension = 0
 function gameLoop() {
 hydrogenNuclei += firstHydrogenDimension*firstHDMultiplier/50
+firstHydrogenDimension += secondHydrogenDimension*secondHDMultiplier/50
 hydrogenNuclei = Math.round( hydrogenNuclei * 50) / 50
+firstHydrogenDimension = Math.round(firstHydrogenDimension * 50) / 50
 document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogenNuclei + " hydrogen nuclei."
+document.getElementById("hydrogenDimensionDisplay").innerHTML = "You have " + firstHydrogenDimension + " hydrogen dimension 1."
 }
 function buyFirstHydrogenDimension() {
   if (hydrogenNuclei >= firstHDCost) {
@@ -20,6 +27,19 @@ function buyFirstHydrogenDimension() {
       document.getElementById("hd1 cost").innerHTML = "a hydrogen dimension 1 costs" + firstHDCost + " hydrogen nuclei."
     }
     document.getElementById("hydrogenDimensionDisplay").innerHTML = "You have " + firstHydrogenDimension + " hydrogen dimension 1."
+  }
+}
+function buyFirstHydrogenDimension() {
+  if (hydrogenNuclei >= secondHDCost) {
+    hydrogenNuclei -= secondHDCost
+    secondHydrogenDimension += 1
+    secondHDTo10 += 1
+    if (secondHDTo10 >= 10) {
+      secondHDTo10 = 0
+      secondHDCost *= 30
+      secondHDMultiplier *= 2
+      document.getElementById("hd2 cost").innerHTML = "a hydrogen dimension 2 costs" + secondHDCost + " hydrogen nuclei."
+    }
   }
 }
 setInterval(gameLoop, 20)
