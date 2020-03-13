246ee0e7f7e9dff21ff30e75fc44bba8686f356b
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2020-03-13 11:33:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,64 @@
-var hydrogenNuclei = 10
-var firstHDCost = 10
-var firstHDMultiplier = 1
-var firstHDTo10 = 0
-var firstHydrogenDimension = 0
-var secondHDCost = 100
-var secondHDMultiplier = 1
-var secondHDTo10 = 0
-var secondHydrogenDimension = 0
+// Put the variables in a game object
+var game = {
+	hydrogenNuclei: 10,
+	firstHDCost: 10,
+	firstHDMultiplier: 1,
+	firstHDTo10: 0,
+	firstHydrogenDimension: 0,
+	secondHDCost: 100,
+	secondHDMultiplier: 1,
+	secondHDTo10: 0,
+	secondHydrogenDimension: 0,
+	// This is for off-window production
+	lastTick: Date.now(),
+}
 function gameLoop() {
-hydrogenNuclei += firstHydrogenDimension*firstHDMultiplier/50
-firstHydrogenDimension += secondHydrogenDimension*secondHDMultiplier/50
-hydrogenNuclei = Math.round( hydrogenNuclei * 50) / 50
-firstHydrogenDimension = Math.round(firstHydrogenDimension * 50) / 50
-document.getElementById("hydrogenDisplay").innerHTML = "You have " + hydrogenNuclei + " hydrogen nuclei."
-document.getElementById("hydrogenDimensionDisplay").innerHTML = "You have " + firstHydrogenDimension + " hydrogen dimension 1."
+	game.hydrogenNuclei += game.firstHydrogenDimension * game.firstHDMultiplier / 50 * (Date.now - game.lastTick) / 1000
+	game.firstHydrogenDimension += game.secondHydrogenDimension * game.secondHDMultiplier / 50 * (Date.now - game.lastTick) / 1000
+	game.hydrogenNuclei = Math.round(game.hydrogenNuclei * 50) / 50
+	game.firstHydrogenDimension = Math.round(game.firstHydrogenDimension * 50) / 50
+	document.getElementById("hydrogenDisplay").innerHTML = "You have " + game.hydrogenNuclei + " hydrogen nuclei."
+	document.getElementById("hydrogenDimensionDisplay").innerHTML = "You have " + game.firstHydrogenDimension + " hydrogen dimension 1."
 }
 function buyFirstHydrogenDimension() {
-  if (hydrogenNuclei >= firstHDCost) {
-    hydrogenNuclei -= firstHDCost
-    firstHydrogenDimension += 1
-    firstHDTo10 += 1
-    if (firstHDTo10 >= 10) {
-      firstHDTo10 = 0
-      firstHDCost *= 10
-      firstHDMultiplier *= 2
-      document.getElementById("hd1 cost").innerHTML = "a hydrogen dimension 1 costs" + firstHDCost + " hydrogen nuclei."
-    }
-    document.getElementById("hydrogenDimensionDisplay").innerHTML = "You have " + firstHydrogenDimension + " hydrogen dimension 1."
-  }
+	if (game.hydrogenNuclei >= game.firstHDCost) {
+		game.hydrogenNuclei -= game.firstHDCost
+		game.firstHydrogenDimension += 1
+		game.firstHDTo10 += 1
+		if (game.firstHDTo10 >= 10) {
+			game.firstHDTo10 = 0
+			game.firstHDCost *= 10
+			game.firstHDMultiplier *= 2
+			document.getElementById("hd1 cost").innerHTML = "a hydrogen dimension 1 costs" + game.firstHDCost + " hydrogen nuclei."
+		}
+	document.getElementById("hydrogenDimensionDisplay").innerHTML = "You have " + game.firstHydrogenDimension + " hydrogen dimension 1."
+	}
 }
 function buySecondHydrogenDimension() {
-  if (hydrogenNuclei >= secondHDCost) {
-    hydrogenNuclei -= secondHDCost
-    secondHydrogenDimension += 1
-    secondHDTo10 += 1
-    if (secondHDTo10 >= 10) {
-      secondHDTo10 = 0
-      secondHDCost *= 30
-      secondHDMultiplier *= 2
-      document.getElementById("hd2 cost").innerHTML = "a hydrogen dimension 2 costs" + secondHDCost + " hydrogen nuclei."
-    }
-  }
+	if (game.hydrogenNuclei >= game.secondHDCost) {
+		game.hydrogenNuclei -= game.secondHDCost
+		game.secondHydrogenDimension += 1
+		game.secondHDTo10 += 1
+		if (game.secondHDTo10 >= 10) {
+			game.secondHDTo10 = 0
+			game.secondHDCost *= 30
+			game.secondHDMultiplier *= 2
+			document.getElementById("hd2 cost").innerHTML = "a hydrogen dimension 2 costs" + game.secondHDCost + " hydrogen nuclei."
+		}
+	}
+}
+
+//Saving
+function save() {
+	let thesave = JSON.stringify(game);
+	thesave = btoa(thesave);
+	localStorage.setItem('elementaldimensonssave', thesave);
+}
+
+//Loading
+function load() {
+	let thesave = localStorage.getItem('elementaldimensonssave');
+	thesave = atob(thesave);
+	game = JSON.parse(thesave);
 }
 setInterval(gameLoop, 20)
